f5dc3c6a8d8a59cc9b6debb29785a316f2480f46
Maksym Havryliuk <maks.havryliuk@gmail.com>
2017-11-13 22:13:49 +0200

    Старт создания веб-приложения Эмулятор кассового места.

---
+++ content.js
@@ -0,0 +1,21 @@
+﻿window.onload = function () {
+
+    /*Запрет ввода букв для поля Цена*/
+    document.getElementById('price').onkeypress = function BanSymbols(e) {
+        return !(/[А-Яа-яA-Za-z-+*/,.]/.test(String.fromCharCode(e.charCode)));
+    };
+
+    /*Запрет ввода букв для поля Количество*/
+    document.getElementById('quantity').onkeypress = function BanSymbols(e) {
+        return !(/[А-Яа-яA-Za-z-+*/,.]/.test(String.fromCharCode(e.charCode)));
+    };
+
+    /*Функция подсчета суммы за один вид товара*/
+    setInterval(function CalcSum() {
+        document.getElementById('result').innerHTML = (parseFloat(document.getElementById('price').value) || 0) * (parseFloat(document.getElementById('quantity').value) || 0);
+    }, 500);
+
+}
+
+
+
