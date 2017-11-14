93ad8e19e0dd1cf538be2fc0a4d06d2c134968d7
Maksym Havryliuk <maks.havryliuk@gmail.com>
2017-11-14 17:19:37 +0200

    Добавил функционал по отправке данных (продукт, цена и количество) на сервер. Немного изменил дизайн.

--- content.js
+++ content.js
@@ -15,7 +15,26 @@
         document.getElementById('result').innerHTML = (parseFloat(document.getElementById('price').value) || 0) * (parseFloat(document.getElementById('quantity').value) || 0);
     }, 500);
 
+    /*Отправка запроса на сервер*/
+    function SubmitForm(product, price, quantity) {
+        $.ajax(
+            {
+                method: "post",
+                url: "/Home/Order",
+                data: { Product: product, Price: price, Quantity: quantity }
+            }).fail(function () {
+                alert("При передаче данных произошла ошибка!");
+            }).done(function (context) { //При успешной отправке получим ответ от сервера
+                alert(context);
+            });
+    }
+
+    $(document).on('submit', 'form', function () {
+        var Product = $('select').val();
+        var Price = $('input[name=Price]').val();
+        var Quantity = $('input[name=Quantity]').val();
+
+        SubmitForm(Product, Price, Quantity);
+    });
 }
-
-
 
